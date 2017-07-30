62c25a0754b941bf6870b6c395bfc33c51f3cb80
Robin Wieruch <wrobin@gmx.net>
2017-07-30 15:26:39 +0200

    normalizr

--- normalizr/index.js
+++ normalizr/index.js
@@ -1,50 +1,32 @@
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+import { schema, normalize } from 'normalizr';
 
-const logger = createLogger();
+const assignedToSchema = new schema.Entity('assignedTo');
 
-function reducer(state, action) {
-  switch(action.type) {
-    case 'TODO_ADD' : {
-      return applyAddTodo(state, action);
-    }
-    case 'TODO_TOGGLE' : {
-      return applyToggleTodo(state, action);
-    }
-    default : return state;
+const todoSchema = new schema.Entity('todo', {
+  assignedTo: assignedToSchema,
+});
+
+const todos = [
+  {
+    id: '0',
+    name: 'create redux',
+    completed: true,
+    assignedTo: {
+      id: '99',
+      name: 'Dan Abramov',
+    },
+  },
+  {
+    id: '1',
+    name: 'create mobx',
+    completed: true,
+    assignedTo: {
+      id: '77',
+      name: 'Michel Weststrate',
+    },
   }
-}
+];
 
-function applyAddTodo(state, action) {
-  const todo = { ...action.todo, completed: false };
-  return [ ...state, todo ];
-}
+const normalizedTodos = normalize(todos, [ todoSchema ]);
 
-function applyToggleTodo(state, action) {
-  return state.map(todo =>
-  todo.id === action.todo.id
-    ? { ...todo, completed: !todo.completed }
-    : todo
-  );
-}
-
-const store = createStore(
-  reducer,
-  [],
-  applyMiddleware(logger)
-);
-
-store.dispatch({
-  type: 'TODO_ADD',
-  todo: { id: '0', name: 'learn redux', completed: false },
-});
-
-store.dispatch({
-  type: 'TODO_ADD',
-  todo: { id: '1', name: 'learn mobx', completed: false },
-});
-
-store.dispatch({
-  type: 'TODO_TOGGLE',
-  todo: { id: '0' },
-});
+console.log(JSON.stringify(normalizedTodos));
